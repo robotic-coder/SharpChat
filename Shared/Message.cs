21d2fd187740183c22940fb9cdf493dd48e03d44
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-11-24 23:53:33 +0200

    Expand Message class

--- Shared/Message.cs
+++ Shared/Message.cs
@@ -1,19 +1,27 @@
 namespace ChatApplication.Shared
 {
-    public abstract class Message
+    public class Message
     {
-		public bool IsSystem { get; }
-		public string Sender { get; }
-		public string Text { get; }
-		public Tuple<int, int, int> Color { get; }
-		public DateTime Time { get; } = DateTime.Now;
+		public string? Id { get; set; }
+		public bool? IsSystem { get; set; }
+		public string? Sender { get; set; }
+		public string? Text { get; set; }
+		public Tuple<int, int, int>? Color { get; set; }
+		public DateTime? Timestamp { get; set; }
+
+		public Message()
+		{
+			// Empty constructor for serialization
+		}
 		
         protected Message(bool isSystem, string sender, string text, Tuple<int, int, int> color)
 		{
+			Id = Guid.NewGuid().ToString();
 			IsSystem = isSystem;
 			Sender = sender;
 			Text = text;
 			Color = color;
+			Timestamp = DateTime.Now;
 		}
     }
 
