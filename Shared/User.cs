69aac19d2f1227fb8d6eb9392c04d60d82b65824
robotic-coder <39992590+robotic-coder@users.noreply.github.com>
2021-11-27 16:41:57 +0200

    Reorganise User class

--- Shared/User.cs
+++ Shared/User.cs
@@ -3,27 +3,27 @@ namespace ChatApplication.Shared
 	public class User
 	{
 		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
 		public string Username { get; set; }
-		public string Name { get; }
-		public string Surname { get; }
 		public Tuple<int, int, int> Color { get; set; }
 
 		public User()
 		{
 			// For serialization. Do not use.
 			this.Id = "";
-			this.Username = "";
 			this.Name = "";
 			this.Surname = "";
+			this.Username = "";
 			this.Color = new Tuple<int, int, int>(0, 0, 0);
 		}
 		
         public User(string name, string surname, string username)
 		{
 			this.Id = Guid.NewGuid().ToString();
-			this.Username = username;
 			this.Name = name;
 			this.Surname = surname;
+			this.Username = username;
 
 			Random rng = new Random();
 			this.Color = new Tuple<int, int, int>(rng.Next(128, 255), rng.Next(128, 255), rng.Next(128, 255)); // Restrict to light colours
